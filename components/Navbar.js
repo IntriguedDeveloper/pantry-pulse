bbe1eee4a13a51d7b02545eac68f11c9d65e0396
Ankit Raj <ruby08241@gmail.com>
2024-03-21 14:55:33 +0530

    initial commit

---
+++ components/Navbar.js
@@ -0,0 +1,15 @@
+
+export default function Navbar() {
+  return (
+    <>
+      <div id="wrapper">
+        <ul>
+          <li id = "brandLogo">
+            <img src = "/navLogo.png"></img>
+          </li>
+          <li id = "brandName">PantryPulse</li>
+        </ul>
+      </div>
+    </>
+  );
+}
