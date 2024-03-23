769aec86ff8deeb87883c0a53862823539d884f8
Ankit Raj <ruby08241@gmail.com>
2024-03-23 14:00:43 +0530

    created basic navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,14 +1,29 @@
-
+import styles from "./Navbar.module.css";
 export default function Navbar() {
   return (
     <>
-      <div id="wrapper">
-        <ul>
-          <li id = "brandLogo">
-            <img src = "/navLogo.png"></img>
-          </li>
-          <li id = "brandName">PantryPulse</li>
-        </ul>
+      <div className={styles.wrapper}>
+        <div className={styles.fSeg}>
+          <ul className={styles.navList}>
+            <li>
+              <img src="/navLogo.png" className={styles.brandLogo}></img>
+            </li>
+            <li className={styles.brandName}>PantryPulse</li>
+          </ul>
+        </div>
+        <div className={styles.sSeg}>
+          <ul className={styles.navList}>
+            <li>
+              <img className = {styles.searchIcon} src="/searchIcon.svg"></img>
+            </li>
+            <li>
+              <img src="/cart.png" className={styles.cartIcon}></img>
+            </li>
+            <li className={styles.profileState}>
+              <button id="loginButton">Login</button>
+            </li>
+          </ul>
+        </div>
       </div>
     </>
   );
