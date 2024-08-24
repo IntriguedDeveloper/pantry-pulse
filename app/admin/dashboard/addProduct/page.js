af8fe11090f78049f329d3687c8553d78297b696
Ankit Raj <ruby08241@gmail.com>
2024-08-24 23:07:30 +0530

    minor changes

--- app/admin/dashboard/addProduct/page.js
+++ app/admin/dashboard/addProduct/page.js
@@ -3,19 +3,22 @@ import { useEffect, useState } from "react";
 import styles from "./page.module.css";
 import Image from "next/image";
 import dropDownIcon from "/public/dropdown.png";
+import clsx from "clsx"; //for multiple classes
 import crossIcon from "/public/crossicon.svg";
 import { getCategories } from "../addProductCategories/handler";
-
+//TODO : add other input fields and complete add product page frontend
+//TODO : style scrollbar
 export default function AddProductPage() {
   const [isVisible, setIsVisible] = useState(false);
   const [categories, setCategories] = useState([]);
   const [isLoading, setIsLoading] = useState([]);
   const [rotate, setRotate] = useState(false); // State to handle rotation
+  const [isChecked, setIsChecked] = useState(false);
   const [isCategorySelected, setIsCategorySelected] = useState({
     status: false,
     categoryName: "",
   });
-  //hello 
+
   useEffect(() => {
     async function fetchCategories() {
       try {
@@ -49,6 +52,11 @@ export default function AddProductPage() {
     setIsVisible(false);
     setRotate(false);
   };
+
+  const handleToggle = () => {
+    setIsChecked(!isChecked);
+  };
+
   return (
     <>
       <link rel="preconnect" href="https://fonts.googleapis.com"></link>
@@ -64,55 +72,105 @@ export default function AddProductPage() {
       <div className={styles.wrapper}>
         <div className={styles.container}>
           <form className={styles.inputForm}>
-            <input
-              placeholder="Enter name of product"
-              type="text"
-              className={styles.inputBox}
-            ></input>
-            <div
-              className={styles.dropDownButton} // Add rotation class conditionally
-              onClick={toggleDropdown}
-            >
-              {isCategorySelected.status ? (
-                isCategorySelected.categoryName
-              ) : (
-                <div
-                  style={{
-                    display: "flex",
-                    alignItems: "center",
-                    justifyContent: "center",
-                  }}
-                >
-                  Select a Category{" "}
-                  <Image
-                    src={dropDownIcon}
-                    className={`${styles.dropDownIcon} ${
-                      rotate ? styles.rotate : ""
-                    }`}
-                    alt="dropdown"
-                  ></Image>{" "}
+            <div className={styles.basicProductInfoContainer}>
+              <h3>Basic Product Info</h3>
+              <input
+                placeholder="Enter name of product"
+                type="text"
+                className={styles.inputBox}
+              ></input>
+              <input placeholder="Enter brand name of product : " className={styles.inputBox}></input>
+              <input placeholder="Enter SKU code : " className={styles.inputBox}></input>
+              <textarea
+                placeholder="Enter product description : "
+                type="text"
+                className={styles.inputDescriptionBox}
+              ></textarea>
+              <div
+                className={styles.dropDownButton} // Add rotation class conditionally
+                onClick={toggleDropdown}
+              >
+                {isCategorySelected.status ? (
+                  "Selected Category : " + isCategorySelected.categoryName
+                ) : (
+                  <div
+                    style={{
+                      display: "flex",
+                      alignItems: "center",
+                      justifyContent: "center",
+                    }}
+                  >
+                    Select a Category{" "}
+                    <Image
+                      src={dropDownIcon}
+                      className={`${styles.dropDownIcon} ${
+                        rotate ? styles.rotate : ""
+                      }`}
+                      alt="dropdown"
+                    ></Image>{" "}
+                  </div>
+                )}
+              </div>
+              {isVisible && (
+                <div className={styles.dropDownOverlayContainer}>
+                  <div className={styles.dropDownDiv}>
+                    <ul className={styles.categoryNames}>
+                      {categories.map((category, index) => (
+                        <li
+                          key={index}
+                          className={styles.category}
+                          onClick={() => {
+                            handleCategorySelection(category);
+                          }}
+                        >
+                          {category}
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
                 </div>
               )}
             </div>
-            {isVisible && (
-              <div className={styles.dropDownOverlayContainer}>
-                <div className={styles.dropDownDiv}>
-                  <ul className={styles.categoryNames}>
-                    {categories.map((category, index) => (
-                      <li
-                        key={index}
-                        className={styles.category}
-                        onClick={() => {
-                          handleCategorySelection(category);
-                        }}
-                      >
-                        {category}
-                      </li>
-                    ))}
-                  </ul>
-                </div>
+            <div className={styles.productPricingInfoContainer}>
+              <h3>Product Pricing Information</h3>
+              <input
+                type="number"
+                placeholder="Enter the price in Rs. : "
+                className={clsx(styles.inputBox, styles.numberInput)}
+                min="0"
+              ></input>{" "}
+              {/*use clsx to apply multiple classnames*/}
+              <input
+                type="number"
+                placeholder="Enter discount percentage : "
+                className={clsx(styles.inputBox, styles.numberInput)}
+                min="0"
+              ></input>
+            </div>
+            <div className={styles.inventoryDetailsContainer}>
+              <h3>Inventory Details</h3>
+              <input
+                type="number"
+                className={clsx(styles.inputBox, styles.numberInput)}
+                placeholder="Enter current stock quantity : "
+              ></input>
+              <div className={styles.checkboxContainer} onClick={handleToggle}>
+                <span
+                  className={`${styles.customCheckbox} ${
+                    isChecked ? styles.checked : ""
+                  }`}
+                >
+                  {isChecked ? "✔" : ""}
+                </span>
+                <label className={styles.checkboxLabel}>
+                  Is product available for Sale ?
+                </label>
               </div>
-            )}
+            </div>
+            <div className={styles.productSpecificationContainer}>
+              <h3>Product Specifications</h3>
+            </div>
+
           </form>
         </div>
       </div>
