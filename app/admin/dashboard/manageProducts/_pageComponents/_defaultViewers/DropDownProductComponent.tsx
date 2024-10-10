c453f32c9f026dcb3ab360a3d0ab1a0c93d07cd1
Ankit Raj <ruby08241@gmail.com>
2024-10-10 12:40:26 +0530

    FIX : fixed below average page styling

--- app/admin/dashboard/manageProducts/_pageComponents/_defaultViewers/DropDownProductComponent.tsx
+++ app/admin/dashboard/manageProducts/_pageComponents/_defaultViewers/DropDownProductComponent.tsx
@@ -1,14 +1,12 @@
 "use client";
-import React, { useState } from "react";
-import { useEffect } from "react";
-
+import React from "react";
 import styles from "./DropDownStyles.module.css";
-import useSWR from "swr";
 import ProductFragment from "./ProductFragment";
 import { ProductDetails } from "../../../addProduct/ClientComponent";
+
 type DropDownProductComponentTypes = {
-  data: any;
-  slicedArray: any;
+  data: ProductDetails[];
+  slicedArray: ProductDetails[];
 };
 
 export default function DropDownProductComponent({
@@ -18,20 +16,52 @@ export default function DropDownProductComponent({
   return (
     <>
       <div className={styles.dropDownContainer}>
-        <div className={styles.titleContainer}>Slaves</div>
-        <div className={styles.listContainer}>
-          {data && data.length > 0
-            ? data.map((product: ProductDetails, index: number) => (
-                <ProductFragment
-                  productImage={product.productImage}
-                  productName={product.productName}
-                  brandName={product.brandName}
-                  stockQuantity={product.stockQuantity}
-                  key={index}
-                ></ProductFragment>
-              ))
-            : "Loading..."}
-        </div>
+        {slicedArray.map(
+          (deDuplicatedObject: ProductDetails, index: number) => (
+            <div className={styles.rowContainer}>
+              <IndividualCategoryRow
+                key={index}
+                categoryName={deDuplicatedObject.selectedCategory}
+                data={data.filter(
+                  (product) =>
+                    product.selectedCategory ===
+                    deDuplicatedObject.selectedCategory
+                )}
+              />
+            </div>
+          )
+        )}
+      </div>
+    </>
+  );
+}
+
+type IndividualCategoryRowProps = {
+  categoryName: string;
+  data: ProductDetails[];
+};
+
+function IndividualCategoryRow({
+  categoryName,
+  data,
+}: IndividualCategoryRowProps) {
+  return (
+    <>
+      <div className={styles.titleContainer}>{categoryName}</div>
+      <div className={styles.listContainer}>
+        {data && data.length > 0 ? (
+          data.map((product: ProductDetails, index: number) => (
+            <ProductFragment
+              key={index}
+              productImage={product.productImage}
+              productName={product.productName}
+              brandName={product.brandName}
+              stockQuantity={product.stockQuantity}
+            />
+          ))
+        ) : (
+          <div>No products available</div>
+        )}
       </div>
     </>
   );
